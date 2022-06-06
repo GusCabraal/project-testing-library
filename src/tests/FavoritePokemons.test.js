bce14592a4985af197f2792f3887bfef03763739
GusCabraal <gustavocabral97@hotmail.com>
2022-06-06 16:59:04 -0300

    requisito 3 concluido mas voltaremos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente FavoritePokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const textEl = screen.getByText(/No favorite pokemon found/i);
+
+    expect(textEl).toBeInTheDocument();
+  });
+});
