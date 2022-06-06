c28842e5a0c109bf2565d3ff0c93c1b6b393bac7
GusCabraal <gustavocabral97@hotmail.com>
2022-06-06 17:05:22 -0300

    requisito 4 concluido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -28,28 +28,4 @@ describe('Testando o componente About', () => {
     expect(imageEl).toBeInTheDocument();
     expect(imageEl).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
-  //   test('Teste se a aplicação é redirecionada ao clicar em um link', async () => {
-  //     const { history } = renderWithRouter(<App />);
-  //     const homeLink = screen.getByRole('link', { name: /home/i });
-  //     const aboutLink = screen.getByRole('link', { name: /about/i });
-  //     const favoritePokeLink = screen.getByRole('link', { name: /favorite/i });
-
-  //     userEvent.click(aboutLink);
-  //     const titleAbout = screen.getByRole('heading', { name: /about/i });
-  //     expect(titleAbout).toBeInTheDocument();
-
-  //     userEvent.click(favoritePokeLink);
-  //     const titleFavorite = screen.getByRole('heading', { name: /favorite/i });
-  //     expect(titleFavorite).toBeInTheDocument();
-
-  //     userEvent.click(homeLink);
-  //     const titleHome = await screen
-  //       .queryByRole('heading', { name: /encountered pokémons/i });
-  //     expect(titleHome).toBeInTheDocument();
-
-//     history.push('/pagina-nao-encontrada');
-//     const titleNotFound = screen
-//       .getByRole('heading', { name: /page requested not found/i });
-//     expect(titleNotFound).toBeInTheDocument();
-//   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente NotFound', () => {
+  test('Teste se contém um heading h2 com o texto Page requested not found ', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundTitle = screen
+      .getByRole('heading', { name: /Page requested not found/i, level: 2 });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<NotFound />);
+    const imageEl = screen
+      .getByAltText(/Pikachu crying because the page requested was not found/i);
+    expect(imageEl).toBeInTheDocument();
+    expect(imageEl).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
