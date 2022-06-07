bfc472d15502f58a2761557a1fa4b02fec49c535
GusCabraal <gustavocabral97@hotmail.com>
2022-06-07 15:04:08 -0300

    ultimo requisito concluido

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,61 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
+
+describe('Testando o componente Pokemon details', () => {
+  const pikachu = pokemons[0];
+  const { name, summary, foundAt } = pikachu;
+
+  test('se as informções detalhadas do pokemon aparecem na tela', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.queryByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+    expect(detailsLink).not.toBeInTheDocument();
+
+    const titleEl = screen.getByRole('heading', { name: `${name} Details` });
+    expect(titleEl).toBeInTheDocument();
+
+    const titleSummary = screen.getByRole('heading', { name: 'Summary', level: 2 });
+    const textSummary = screen.getByText(summary);
+    expect(titleSummary).toBeInTheDocument();
+    expect(textSummary).toBeInTheDocument();
+  });
+
+  test('se existe uma seção com os mapas contendo as localizações do pokémon', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.queryByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const titleEl = screen
+      .getByRole('heading', { name: `Game Locations of ${name}`, level: 2 });
+    expect(titleEl).toBeInTheDocument();
+
+    const locationImages = screen.getAllByAltText(`${name} location`);
+    foundAt.forEach(({ location, map }, index) => {
+      const locationName = screen.getByText(location);
+      expect(locationName).toBeInTheDocument();
+      expect(locationImages[index]).toBeInTheDocument();
+      expect(locationImages[index]).toHaveAttribute('src', map);
+    });
+  });
+  test('se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.queryByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+
+    const checkbox = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+
+    const starPoke = screen.queryByAltText(`${name} is marked as favorite`);
+    expect(starPoke).toBeInTheDocument();
+    expect(starPoke).toHaveAttribute('src', '/star-icon.svg');
+
+    userEvent.click(checkbox);
+    expect(checkbox).not.toBeChecked();
+    expect(starPoke).not.toBeInTheDocument();
+  });
+});
